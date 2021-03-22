d652ed7a852c5d1e47ed136a110a5507772a017d
water-racoon <70622203+water-racoon@users.noreply.github.com>
2021-03-22 22:57:33 +0900

    Add files via upload

---
+++ scrt.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $(".card-body").on('click',function (event) {
+    $(this).parent().animate({
+      opacity:"0"
+    },100);
+    $(".popup").fadeIn();
+    $(".popmenu").html($(this).parent().html()).appendTo(".popup")
+  });
+  $(".popup").on('click',function (event) {
+    $(".popup").fadeOut();
+    $(".card-main").css('opacity','1');
+  });
+  $('header').find('h1').mouseenter(function(event){
+    $(this).each(function(index,element){
+      var pleace = $('.mainbar').attr('class');
+      var count = $('.mainbar').length;
+      $(this).text(count+"tag in "+pleace);
+    });
+  });
+  $('header').find('h1').mouseleave(function(event){
+    $(this).each(function(index,element){
+      $(this).text("homepage main");
+    });
+  });
+  $(".mainbar").onclick(function(){
+    document.location.href='201868007_.html'
+  })
+});
